6c1edf2ba1dadc0c08ef91eee66ef62bdc15823e
kohl <vitoria.azvedo@estudante.ifms.edu.br>
2025-10-20 21:43:29 -0400

    Adicionando o projeto maven ao repositorio remoto

---
+++ tplp332111/src/main/java/br/edu/ifms/cinema/Tplp332111.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifms.cinema;
+
+/**
+ *
+ * @author Estudante
+ */
+public class Tplp332111 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
